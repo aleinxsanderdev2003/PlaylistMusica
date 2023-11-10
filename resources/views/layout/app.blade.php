14544be1dfb5964c5d72a96d2534edeb121ba729
aleinxsanderdev2003 <alexander_03c@outlook.es>
2023-11-09 22:45:02 -0500

    avance 2

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -13,9 +13,72 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
+    <!--Stilos-->
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
+
+    <div class="bg__container">
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img  src="img/logos/logo.png" width="30" height="30" class="d-inline-block align-top" alt="Logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div   class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contacto">Contacto</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Menú</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fab fa-facebook-f"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fab fa-twitter"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fab fa-instagram"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+        <div class="texto" id="home">
+            <h4 class="text-white" style="font-family: 'Playfair Display', serif;">Playlist Web</h4>
+            <h3 class="text-white" style="font-family: 'Quicksand', sans-serif;">Restaurante</h3>
+            <a href="" class="btn text-white"  style="font-family: 'Quicksand', sans-serif; font-size:15px;background: rgb(15, 28, 70">Ordenar</a>
+        </div>
+
+
+
+
+
+<script>
+    $(window).scroll(function() {
+    if ($(this).scrollTop() > 0) {
+        $('.navbar').addClass('scrolled');
+    } else {
+        $('.navbar').removeClass('scrolled');
+    }
+});
+<div class="bg-black text-white text-center">
+
+&copy; 2023 - Todos los derechos reservados (Desarrollado por Gimmick)
+
+</div>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+
 
 </body>
 </html>
