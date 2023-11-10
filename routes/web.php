1fedc3e6a1b3f30b66f2ee1c38ac243d55391710
richard cabanillas <rixar2433@gmail.com>
2023-11-09 23:29:50 -0500

    actualizacion richard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layout.app');
 });
+
+Route::get('/playlist', function () {
+    return view('playList');
+});
